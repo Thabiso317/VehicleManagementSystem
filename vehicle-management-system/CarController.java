5736399b6e6cd3caa89355458b5859fb85b71453
Thabiso317 <maekothabiso@gmail.com>
2024-11-10 10:58:50 +0200

    Task 2A: Users service implementation

--- vehicle-management-system/CarController.java
+++ vehicle-management-system/CarController.java
@@ -1,3 +1,37 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cars")
 public class CarController {
 
+    @Autowired
+    private CarService carService;
+
+    @GetMapping
+    public List<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    @GetMapping("/{id}")
+    public Car getCarById(@PathVariable Long id) {
+        return carService.getCarById(id);
+    }
+
+    @PostMapping
+    public Car addCar(@RequestBody Car car) {
+        return carService.addCar(car);
+    }
+
+    @PutMapping("/{id}")
+    public Car updateCar(@PathVariable Long id, @RequestBody Car car) {
+        return carService.updateCar(id, car);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id) {
+        carService.deleteCar(id);
+    }
 }
--- vehicle-management-system/CarServiceImpl.java
+++ vehicle-management-system/CarServiceImpl.java
@@ -1,3 +1,48 @@
-public class CarServiceImpl {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Override
+    public List<Car> getAllCars() {
+        return carRepository.findAll();
+    }
+
+    @Override
+    public Car getCarById(Long id) {
+        Optional<Car> car = carRepository.findById(id);
+        return car.orElse(null);
+    }
+
+    @Override
+    public Car addCar(Car car) {
+        return carRepository.save(car);
+    }
+
+    @Override
+    public Car updateCar(Long id, Car car) {
+        if (carRepository.existsById(id)) {
+            car.setId(id);
+            return carRepository.save(car);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteCar(Long id) {
+        carRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Car> getAvailableCars() {
+        return carRepository.findByAvailable(true);
+    }
 }
